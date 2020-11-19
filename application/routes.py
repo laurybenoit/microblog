92bb748e72e0275cf641626a967220bc0c3d70c5
laurybenoit <lauryalexandre@me.com>
2020-11-19 01:53:02 -0500

    working

--- application/routes.py
+++ application/routes.py
@@ -6,6 +6,9 @@ from flask_login import current_user, login_user
 from application.models import User
 from flask_login import logout_user
 from flask_login import login_required
+from flask import request
+from werkzeug.urls import url_parse
+
 
 
 
@@ -31,10 +34,20 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    # if form.validate_on_submit():
+    #     flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+    #     return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
-    
+
 
 @app.route('/logout')
 def logout():
